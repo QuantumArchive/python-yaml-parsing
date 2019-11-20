6fcf06e937e23a4e64ec7fdf0078911f2514da92
QuantumArchive <chrbruner9@gmail.com>
2019-11-20 09:54:57 -0800

    description is optional

--- index.py
+++ index.py
@@ -11,8 +11,9 @@ def flatten(fileDict):
         for k1, v1 in value['end_ports'].items():
             row = copy.deepcopy(v1)
             row['recordId'] = key
-            row['description'] = value['description']
             row['end_port_name'] = k1
+            if value.get('description') != None:
+                row['description'] = value['description']
             flattened_data.append(row)
     return flattened_data
 
