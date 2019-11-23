44b587950673d6c7679fc74b811b4b53a518872c
QuantumArchive <chrbruner9@gmail.com>
2019-11-23 01:27:28 -0800

    better naming of variables, end port circuits is ignored

--- index.py
+++ index.py
@@ -7,17 +7,20 @@ import pandas
 
 def flatten(fileDict):
     flattened_data = []
-    for key, value in fileDict.items():
-        for k1, v1 in value['end_ports'].items():
-            row = copy.deepcopy(v1)
-            row['recordId'] = key
-            row['end_port_name'] = k1
+
+    for recordId, record in fileDict.items():
+        for end_port, end_port_record in record['end_ports'].items():
+
+            if end_port == 'circuits':
+                continue
+
+            row = copy.deepcopy(end_port_record)
+            row['recordId'] = recordId
+            row['end_port_name'] = end_port
 
             # Cleaning out data
-            if value.get('description') != None:
-                row['description'] = value['description']
-            if row.get('circuits') != None:
-                row.pop('circuits')
+            if record.get('description') != None:
+                row['record_description'] = record['description']
 
             flattened_data.append(row)
     return flattened_data
@@ -27,13 +30,13 @@ def getDataFrame(collection):
 
 def main():
     # going to have to supply with the name of the yaml you care about
-    
+    fileName = 'Untitled2'
     cwd = os.getcwd()
-    with open("%s/assets/Untitled.yaml" % cwd) as file:
+    with open("%s/assets/%s.yaml" % (cwd, fileName)) as file:
         pythonized_yaml = yaml.full_load(file)
         flattened_data = flatten(pythonized_yaml)
         data_frame = getDataFrame(flattened_data)
-        data_frame.to_csv("%s/assets/Untitled.csv" % cwd)
+        data_frame.to_csv("%s/assets/%s.csv" % (cwd, fileName))
 
 if __name__ == "__main__":
     main()
